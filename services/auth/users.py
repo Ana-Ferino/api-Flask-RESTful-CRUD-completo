e491c9a5eedbe677e19c025882b0a90d362ea4b6
Ana-Ferino <anajulia.ferino@outlook.com>
2023-09-10 15:07:52 -0300

    feat: adiciona método de validação de token

--- services/auth/users.py
+++ services/auth/users.py
@@ -1,9 +1,25 @@
+from controller.login_controller import LoginDTO
 from models.database import db_session
 from models.usuarios import Usuarios
 
 
+def find_user_by_email(login: LoginDTO):
+    usuario = Usuarios.query.filter_by(email=login.usuario).first()
+
+    if usuario:
+        return usuario
+    
+    return None
+
 def get_users_data() -> list[dict]:
     query_results_users = Usuarios.query.all()
-    dados_por_usuario = [{'id': i.id, 'login': i.login, 'senha': i.senha} for i in query_results_users]
+    dados_por_usuario = [
+        {
+            'id': i.id, 
+            'login': i.login,
+            'senha': i.senha
+        } 
+        for i in query_results_users
+    ]
     db_session.close()
     return dados_por_usuario
--- services/auth/verify_login.py
+++ services/auth/verify_login.py
@@ -1,21 +1,26 @@
-from flask_httpauth import HTTPBasicAuth
-from services.auth.users import get_users_data
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from dtos.login import LoginDTO
+from services.auth.users import find_user_by_email
+from services.auth.redis_data import get_token_by_user_id
 import bcrypt
 
 auth = HTTPBasicAuth()
 
-
 @auth.verify_password 
-def validate_login_input(login, senha):
+def validate_login_input(usuario, senha) -> bool:
     password_encoded = senha.encode('utf-8')
+    user = find_user_by_email(LoginDTO(usuario, senha))
 
-    users = get_users_data()
-    for i in users:
-        if login in i.values():
-            password_user = i.get('senha')
+    if user:
+        validated = bcrypt.checkpw(password=password_encoded, hashed_password=user.senha)
+        return validated
+    
+    return False
 
-    validated = bcrypt.checkpw(password=password_encoded, hashed_password=password_user)
-    if validated:
-        return True
-    else:
-        return False
+
+auth_with_token = HTTPTokenAuth()
+
+@auth_with_token.verify_token
+def validate_token(token, id_usuario) -> bool:
+    token_bd = get_token_by_user_id(id_usuario)
+    return token == token_bd
