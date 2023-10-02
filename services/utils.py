176e05f3f9db29b65fb896b76663debf57893061
Ana-Ferino <anajulia.ferino@outlook.com>
2023-10-02 00:27:47 -0300

    refact: método encrypt-password

--- services/utils.py
+++ services/utils.py
@@ -1,6 +1,7 @@
 import bcrypt
 
-def criptografar_senha(senha: str) -> bytes:
+
+def encrypt_password(senha: str) -> bytes:
     salt = bcrypt.gensalt()
     password_encoded = senha.encode('utf-8')
     hash = bcrypt.hashpw(password=password_encoded, salt=salt)
