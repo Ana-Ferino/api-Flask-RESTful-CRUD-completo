300fe535bb3cda53eb3766896d80dbb172a6aa6d
Ana-Ferino <anajulia.ferino@outlook.com>
2023-10-02 00:30:07 -0300

    feat: adiciona rota signup

--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 from flask_restful import Api
 from controller.atividade_controller import AtividadeController
 from controller.login_controller import LoginController
+from controller.signup_controller import SignUpController
 from controller.pessoa_controller import PessoasController
 from controller.usuario_controller import UsuariosController
 
@@ -9,4 +10,5 @@ def setup_routes(api: Api):
     api.add_resource(PessoasController, '/pessoas/')
     api.add_resource(AtividadeController, '/atividades/')
     api.add_resource(UsuariosController, '/usuarios/')
+    api.add_resource(SignUpController, '/sign-up/')
     api.add_resource(LoginController, '/login/')
