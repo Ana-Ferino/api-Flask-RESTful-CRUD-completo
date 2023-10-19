867c7e51b5ef2abab0594de649769a99faa97f77
Ana-Ferino <anajulia.ferino@outlook.com>
2023-10-19 00:06:14 -0300

    feat: implementa testes funcionais

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import os
+import pytest
+from app import app
+
+
+@pytest.fixture(scope='module')
+def test_client():
+
+    os.environ['CONFIG_TYPE'] = 'config.TestingConfig'
+    flask_app = app
+
+    with flask_app.test_client() as testing_client:
+        
+        with flask_app.app_context():
+            yield testing_client
