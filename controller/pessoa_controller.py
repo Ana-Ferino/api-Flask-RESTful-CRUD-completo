7c1bb57f896cafac635b4df0bef65d153295d2b2
Ana-Ferino <anajulia.ferino@outlook.com>
2024-01-21 15:23:15 -0300

    refact: controller de Pessoas

--- controller/pessoa_controller.py
+++ controller/pessoa_controller.py
@@ -1,30 +1,86 @@
 from services.pessoa_services import PessoasServices
-from services.auth.verify_login import auth
+from services.auth.sessions import SessaoServices
+from exceptions import PersonAlreadyExistsError, PersonNotExistsError, UnauthorizedModification
+from dtos.login import LoginDTO
+from dtos.person import PersonDTO
 from flask_restful import Resource
-from flask import request
+from pydantic import ValidationError
+from flask import request, current_app
 
 
 class PessoasController(Resource):
     def get(self):
-        pessoas = PessoasServices.get()
-        return pessoas
+        try:
+            uri_parameters = PersonDTO(**request.args.to_dict())
+            auth = LoginDTO(token=request.headers.get('Authorization'))
+            same_user = SessaoServices().validate_user_on_request(auth, uri_parameters)
+
+            if same_user:
+                person = PessoasServices().get(name=uri_parameters.name)
+
+                current_app.logger.info("GET performed by the user : {}".format(person.nome))
+                return 'Name: {} - Age: {}'.format(person.nome, person.idade), 200
+        except Exception as error:
+            return 'An error occurred: {}'.format(error), 500
     
-    @auth.login_required
     def post(self):
-        dados = request.json
-        retorno_adicao_de_registro = PessoasServices.save(nome=dados['nome'], idade=dados['idade'])
-        return retorno_adicao_de_registro
+        try:
+            payload = request.get_json()
+            auth = LoginDTO(token=request.headers.get('Authorization'))
 
-    @auth.login_required
-    def put(self):
-        dados = request.json
-        retorno_modificacao = PessoasServices.modify(id=dados['id'], 
-                                                     nome=dados['nome'], 
-                                                     idade=dados['idade'])
-        return retorno_modificacao
+            person = PersonDTO(**payload)
+            current_app.logger.info("POST activity performed by the user : {}".format(auth.Usuario))
 
-    @auth.login_required
+            PessoasServices().save(person)
+            return 'Successfully registered person: {}'.format(person.name), 201
+        
+        except PersonAlreadyExistsError as pae:
+            return 'ERROR: {}'.format(pae), 422
+        except ValidationError as error:
+            return {'errors': error.errors()}, 420
+        except Exception as excp:
+            return 'An error occurred: {}'.format(excp), 500
+
+    def patch(self):
+        try:
+            uri_parameters = PersonDTO(**request.args.to_dict())
+            auth = LoginDTO(token=request.headers.get('Authorization'))
+            same_user = SessaoServices().validate_user_on_request(auth, uri_parameters)
+            
+            if same_user:
+                person_ty_modify = uri_parameters.name
+                new_data = request.environ.get('json', None)
+                PessoasServices().modify(person_ty_modify, new_data=PersonDTO(**new_data))
+
+                message = 'Person {} modified successfully.'.format(person_ty_modify)
+                current_app.logger.info(message)
+                return message, 200
+            
+        except PersonAlreadyExistsError as pae:
+            return 'ERROR: {}'.format(pae), 422
+        except PersonNotExistsError as pne:
+            return '{}'.format(pne), 500
+        except UnauthorizedModification as um:
+            return '{}'.format(um), 401
+        except Exception as error:
+            return 'An error occurred: {}'.format(error), 500
+
     def delete(self):
-        dados = request.json
-        retorno_exclusao = PessoasServices.delete(id=dados['id'])
-        return retorno_exclusao
+        try:
+            uri_parameters = PersonDTO(**request.args.to_dict())
+            auth = LoginDTO(token=request.headers.get('Authorization'))
+            same_user = SessaoServices().validate_user_on_request(auth, uri_parameters)
+
+            if same_user:
+                PessoasServices().delete(person=uri_parameters)
+                
+                message = 'Person {} successfully deleted.'.format(uri_parameters.name)
+                current_app.logger.info(message)
+                return message, 200
+        
+        except PersonNotExistsError as pae:
+            return '{}'.format(pae), 500
+        except UnauthorizedModification as um:
+            return '{}'.format(um), 401
+        except Exception as error:
+            return 'An error occurred: {}'.format(error), 500
