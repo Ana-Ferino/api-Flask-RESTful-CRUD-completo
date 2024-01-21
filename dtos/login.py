4ccde1da85f0691dee9228fb53fa1d3af5e82089
Ana-Ferino <anajulia.ferino@outlook.com>
2024-01-21 15:18:31 -0300

    refact: deleta field desnecessario no LoginDTO

--- dtos/login.py
+++ dtos/login.py
@@ -1,11 +1,6 @@
 from pydantic import BaseModel
-from pydantic import Field
-
 
 class LoginDTO(BaseModel):
-    Usuario: str = Field(default=None, alias='usuario')
-    Senha: str = Field(default=None, alias='senha')
-    Token: str = Field(default=None, alias='token')
-
-    class Config:
-        populate_by_name =  True
+    Usuario: str = None
+    Senha: str = None
+    Token: str = None
